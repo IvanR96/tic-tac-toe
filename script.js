34323d3fde2feea2bf855eef1dd30c81cfe4de32
IvanR96 <irios96@hotmail.com>
2023-12-12 22:18:15 -0800

    setting up logic

--- script.js
+++ script.js
@@ -1,70 +1,41 @@
-const GameModule = (function() {
-    // Player factory
-    const createPlayer = (name, symbol) => ({ name, symbol });
+const player = (function(){
+    (name,symbol) => ({name, symbol});
 
-    // Gameboard factory
-    const createGameboard = () => {
-        const board = ['', '', '', '', '', '', '', '', ''];
+});
 
-        return {
-            getBoard: () => [...board],
-            updateCell: (index, symbol) => {
-                if (board[index] === '') {
-                    board[index] = symbol;
-                    return true; // Cell updated successfully
-                }
-                return false; // Cell already occupied
-            },
-            // Other methods for managing the game board can be added here
-        };
+
+const gameBoard = ({
+    const board = ['','','','','','','','',''];
+
+    const getBoard = () => [...board];
+
+    const updateCell = (index, symbol) =>{
+        if (board[index]=== ''){
+            board[index] = symbol;
+            return true;
+        }
+        return false;
+
     };
 
-    // Game factory
-    const createGame = () => {
-        let currentPlayer = null;
-        const players = [];
 
-        const startGame = () => {
-            // Initialize players
-            const player1 = createPlayer('Player 1', 'X');
-            const player2 = createPlayer('Player 2', 'O');
-            players.push(player1, player2);
+    const checkWin = () => {
 
-            // Set the starting player
-            currentPlayer = player1;
-
-            // Call a function to initialize the game board UI
-            initializeBoardUI       };
-
-        const createBoard = () => {
-            const gameBoardElement = document.getElementById('game-board');
-
-            // Create buttons for each cell
-            for (let i = 0; i < 9; i++) {
-                const cellButton = document.createElement('button');
-                cellButton.setAttribute('data-index', i);
-                cellButton.addEventListener('click', () => handleCellClick(i));
-                gameBoardElement.appendChild(cellButton);
-            }
-        };
-
-        const handleCellClick = (index) => {
-            // Handle the cell click event
-            // Add game logic here to update the Gameboard and check for a winner
-            // You can use gameboard.getBoard() to access the game board array
-            // Update the UI accordingly
-        };
-
-        return {
-            startGame,
-        };
     };
 
-    return {
-        createGame,
+
+    const checkTie = () => {
+
+    };
+
+    const resetBoard = () =>{
+        for(let i = 0; i < board.length; i++){
+            board[i] = '';
+        }
+    };
+    
+    return {getBoard,
+    updateCell, checkWin,
+    checkTie,resetBoard
     };
 })();
-
-// Start the game
-const game = GameModule.createGame();
-game.startGame();
